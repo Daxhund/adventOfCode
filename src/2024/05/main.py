4cd7c37d6553133a1630b0327b5e8089534c55d9
Daxhund <daxhund.message@gmail.com>
2024-12-09 22:16:19 +0100

    linting

--- src/2024/05/main.py
+++ src/2024/05/main.py
@@ -24,7 +24,7 @@ class Manual:
         return [id for id, value in enumerate(self.pages) if value == page_id]
 
     def get_middle_value(self) -> int:
-        offset: int = math.ceil(len(self.pages) / 2) -1
+        offset: int = math.ceil(len(self.pages) / 2) - 1
         return self.pages[offset]
 
     def __str__(self):
@@ -45,24 +45,24 @@ class ManualValidator:
         for rule in self.rulset:
             before: List[int] = manual.get_indexes_by_page_id(rule.a)
             after: List[int] = manual.get_indexes_by_page_id(rule.b)
-            #print(f"rule{rule} on {manual}")
+            # print(f"rule{rule} on {manual}")
             before = sorted(before)
             after = sorted(after)
-            #print(f"before{before}")
-            #print(f"after{after}")
+            # print(f"before{before}")
+            # print(f"after{after}")
             if not (before and after):
-                #print("missing index")
+                # rule not applicable
                 continue
-
-            if before[0] > after[0]:
+            validity = before[0] > after[0]
+            if validity:
                 is_valid = False
-                #print(f"Manual {manual} is invalid due to {rule}")
+                # print(f"Manual {manual} is invalid due to {rule}")
                 self.invalid_manuals.append(manual)
                 break
         middle_value: int = 0
         if is_valid is True:
             middle_value = manual.get_middle_value()
-            #print(f"Manual {manual} is valid and middle value is {middle_value}")
+            # print(f"Manual {manual} is valid and middle value is {middle_value}")
 
         return middle_value
 
@@ -88,13 +88,54 @@ class ManualValidator:
                 pages = [int(line) for line in line.split(",")]
                 manuals.append(Manual(pages))
         return manuals
-    
-    def __fix_manual(self, manual:Manual) -> int:
-        print(manual)
-        return 0
 
+    def modify_manual(self, rule: Rule, manual: Manual) -> Manual:
+        before: int = manual.get_indexes_by_page_id(rule.a)[0]
+        after: int = manual.get_indexes_by_page_id(rule.b)[0]
+        manual.pages[before], manual.pages[after] = (
+            manual.pages[after],
+            manual.pages[before],
+        )
+        print(f"{after} <-> {before}")
+        return manual
 
+    def __fix_manual(self, manual: Manual) -> int:
+        manual_local = manual
+        iterator = iter(self.rulset)
+        while True:
+            try:
+                rule = next(iterator)
 
+                if not self.rule_valid(manual_local, rule):
+                    manual_local = self.modify_manual(rule, manual_local)
+                    print("reset")
+                    iterator = iter(self.rulset)
+                print(f"{manual} <-- {rule}")
+            except StopIteration:
+                print("fixed")
+                break
+
+            except ValueError:
+                continue
+
+        return manual_local.get_middle_value()
+
+    def rule_valid(self, manual, rule) -> bool:
+        is_valid: bool = True
+        before: List[int] = manual.get_indexes_by_page_id(rule.a)
+        after: List[int] = manual.get_indexes_by_page_id(rule.b)
+        # print(f"rule{rule} on {manual}")
+        before = sorted(before)
+        after = sorted(after)
+        # print(f"before{before}")
+        # print(f"after{after}")
+        if not (before and after):
+            # rule not applicable
+            raise ValueError("not applicable")
+        validity = before[0] > after[0]
+        if validity:
+            is_valid = False
+        return is_valid
 
     def sanitize_invalid_manuals(self) -> str:
         magic_number: int = 0
@@ -108,7 +149,7 @@ def test() -> None:
         data = fh.readlines()
         mv = ManualValidator(data)
         print(mv.validate_manuals())
-        #mv.sanitize_invalid_manuals()
+        print(mv.sanitize_invalid_manuals())
 
 
 if __name__ == "__main__":
